075d4d3f98514f8e841ffdae7066557ad6ce6201
LaHoangLoc <laloc2496@gmail.com>
2017-07-19 12:56:53 +0700

    Add validate, inputAction into URL_Controller and hasKey into URL_Model

--- Models/Access_Model.php
+++ Models/Access_Model.php
@@ -1,10 +1,10 @@
 <?php
     if ( ! defined('PATH_MODEL')) die ('Bad requested!');
-    include_once PATH_MODEL . '\Base_Model.php'; 
+    include_once PATH_MODEL . '\Base_Model.php';
     class Access_Model extends Base_Model{
         function __construct () {
             parent::__construct();
         }
    }
-    
+
 ?>
