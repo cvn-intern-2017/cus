aa14dcb46b5d7f841063d3687f4367fc295ba294
Nguyen Thi Thanh Hang <h001012@cb.local>
2017-07-19 10:26:02 +0700

    UI

---
+++ UI/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="ISO-8859-1">
+    <title>ShortenURL</title>
+    <link rel="stylesheet" href="css/materialize.css">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+<nav>
+    <div class="nav-tabs">
+        <a href="#" class="brand-logo">Logo</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="row">
+        <form class="col s12">
+            <div class="row">
+
+                <div class="input-field col s6">
+                    <input placeholder="Your original URL here" id="first_name" type="text" class="validate">
+                    <a class="waves-effect waves-light btn" id="myBtn">Shorten</a>
+
+                </div>
+            </div>
+
+        </form>
+    </div>
+</div>
+
+<div id="myModal" class="modal">
+
+    <!-- Modal content -->
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <p class="txt">Your Short URL</p>
+        <div class="mydiv2">
+            <p class="text">cybozu.vn/V2pMaf</p>
+        </div>
+        <div class="div-btnDone">
+            <a class="waves-effect waves-light btn" id="btnDone">Done</a>
+        </div>
+
+    </div>
+
+</div>
+<script src="myjs.js"></script>
+
+</body>
+</html>
