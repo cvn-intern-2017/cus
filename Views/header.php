1e5403e8b14a48894e071aa017e742d072d0b392
LaHoangLoc <laloc2496@gmail.com>
2017-07-21 15:47:46 +0700

    update .htaccess

--- Views/header.php
+++ Views/header.php
@@ -9,10 +9,10 @@
 </head>
 <body>
 
-  <nav>
+  <nav class="light-blue lighten-1">
     <div class="container">
       <div class="nav-wrapper">
-        <a href="#" class="brand-logo">Cybozu</a>
+        <a href="#" class="brand-logo">Cybozu <h10 style="font-size:60%;"> URL Shortener</h10></a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
           <li><a href=".">Home</a></li>
           <li><a href="badges.html">Components</a></li>
