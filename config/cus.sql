77b96419cd0fd86cbc047989c60a92697c953a88
Nam <namnguyen01071995@gmail.com>
2017-07-26 14:51:49 +0700

    new file .sql

--- config/cus.sql
+++ config/cus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 26, 2017 at 09:47 AM
+-- Generation Time: Jul 26, 2017 at 09:50 AM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.1.7
 
