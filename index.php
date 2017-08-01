972b170d0dfc14262783ccb7308d7c1332b6fda0
LaHoangLoc <laloc2496@gmail.com>
2017-08-01 15:32:40 +0700

    update autoload() function

--- index.php
+++ index.php
@@ -1,18 +1,35 @@
 <?php
     define('DOMAIN','http://cus.dev.cybozu.xyz/');
-    define('PATH_CONTROLLER',__DIR__ . '/Controllers');
-    define('PATH_VIEW',__DIR__ . '/Views');
-    define('PATH_MODEL',__DIR__ . '/Models');
-    define('PATH_LIB',__DIR__ . '/lib');
-    define('PATH_SMARTY',__DIR__ .'/smarty');
+    define('PATH_CONTROLLER',__DIR__ . '/Controllers/');
+    define('PATH_VIEW',__DIR__ . '/Views/');
+    define('PATH_MODEL',__DIR__ . '/Models/');
+    define('PATH_LIB',__DIR__ . '/lib/');
+    define('PATH_SMARTY',__DIR__ .'/smarty/');
     define('ARR_INI',parse_ini_file('config/config.ini',true));
     define('INI_DATABSE',ARR_INI['database']);
-    include_once PATH_SMARTY . '/Cus_Smarty.php';
-    include_once PATH_CONTROLLER . '/URL_Controller.php';
-    include_once PATH_CONTROLLER . '/Access_Controller.php';
-    include_once PATH_LIB . '/Browser.php';
-    include_once PATH_LIB . '/Utils.php';
-    include_once PATH_MODEL . '/URL_Model.php';
+    include_once PATH_LIB . 'Utils.php';
+
+    function __autoload($className){
+        if(preg_match('/(_Model)$/',$className)) {
+            $pathFile =  PATH_MODEL . $className . '.php';
+        }
+        else if(preg_match("/(_Controller)$/",$className)) {
+            $pathFile =  PATH_CONTROLLER . $className . '.php';
+
+        }
+        else if($className === 'Browser'){
+            $pathFile = PATH_LIB . 'Browser.php';
+        }
+        else if($className === 'Cus_Smarty') {
+            $pathFile = PATH_SMARTY . 'Cus_Smarty.php';
+        }
+        else {
+            $pathFile = "";
+        }
+        if(file_exists($pathFile)){
+            include_once $pathFile;
+        }
+    }
 
     // Trường hợp vào giao diện input URL để nhận shortlink.
     if ($_SERVER['REQUEST_URI'] === '/' || $_SERVER['REQUEST_URI'] === '/index.php') { // cybozu.xyz/
