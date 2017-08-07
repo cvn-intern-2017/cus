b67522ad708bbfaf3b4064fc75e24914b0a051f0
LaHoangLoc <laloc2496@gmail.com>
2017-08-07 11:36:21 +0700

    update new database design

--- config/cus.sql
+++ config/cus.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 28, 2017 at 09:06 AM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 7.1.7
+-- Generation Time: Aug 07, 2017 at 06:28 AM
+-- Server version: 10.1.24-MariaDB
+-- PHP Version: 7.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -22,6 +22,105 @@ SET time_zone = "+00:00";
 -- Database: `cus`
 --
 
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_test` (`var1` INT)  BEGIN   
+    DECLARE start  INT unsigned DEFAULT 1;  
+    DECLARE finish INT unsigned DEFAULT 10;
+
+    SELECT  var1, start, finish;
+
+    SELECT * FROM places WHERE place BETWEEN start AND finish; 
+END$$
+
+--
+-- Functions
+--
+CREATE DEFINER=`root`@`localhost` FUNCTION `convert_10_to_62` (`id_num` INT) RETURNS VARCHAR(6) CHARSET latin1 BEGIN
+	DECLARE key_url VARCHAR(6);
+    DECLARE remainder INT;
+    SET key_url = '';
+	WHILE id_num > 0 DO
+        SET remainder = id_num % 62;
+        SET id_num = ROUND(id_num / 62 - 0.5);
+        SET key_url = CONCAT(convert_one_char(remainder), key_url);
+  	END WHILE;
+    RETURN key_url;
+END$$
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `convert_one_char` (`i` INT) RETURNS VARCHAR(1) CHARSET latin1 BEGIN
+    DECLARE result VARCHAR(1);
+	CASE i 
+        WHEN 0 THEN SET result = '0';
+        WHEN 1 THEN SET result = '1';
+        WHEN 2 THEN SET result = '2';
+        WHEN 3 THEN SET result = '3';
+        WHEN 4 THEN SET result = '4';
+        WHEN 5 THEN SET result = '5';
+        WHEN 6 THEN SET result = '6';
+        WHEN 7 THEN SET result = '7';
+        WHEN 8 THEN SET result = '8';
+        WHEN 9 THEN SET result = '9';
+        WHEN 10 THEN SET result = 'a';
+        WHEN 11 THEN SET result = 'b';
+        WHEN 12 THEN SET result = 'c';
+        WHEN 13 THEN SET result = 'd';
+        WHEN 14 THEN SET result = 'e';
+        WHEN 15 THEN SET result = 'f';
+        WHEN 16 THEN SET result = 'g';
+        WHEN 17 THEN SET result = 'h';
+        WHEN 18 THEN SET result = 'i';
+        WHEN 19 THEN SET result = 'j';
+        WHEN 20 THEN SET result = 'k';
+        WHEN 21 THEN SET result = 'l';
+        WHEN 22 THEN SET result = 'm';
+        WHEN 23 THEN SET result = 'n';
+        WHEN 24 THEN SET result = 'o';
+        WHEN 25 THEN SET result = 'p';
+        WHEN 26 THEN SET result = 'q';
+        WHEN 27 THEN SET result = 'r';
+        WHEN 28 THEN SET result = 's';
+        WHEN 29 THEN SET result = 't';
+        WHEN 30 THEN SET result = 'u';
+        WHEN 31 THEN SET result = 'v';
+        WHEN 32 THEN SET result = 'w';
+        WHEN 33 THEN SET result = 'x';
+        WHEN 34 THEN SET result = 'y';
+        WHEN 35 THEN SET result = 'z';
+        WHEN 36 THEN SET result = 'A';
+        WHEN 37 THEN SET result = 'B';
+        WHEN 38 THEN SET result = 'C';
+        WHEN 39 THEN SET result = 'D';
+        WHEN 40 THEN SET result = 'E';
+        WHEN 41 THEN SET result = 'F';
+        WHEN 42 THEN SET result = 'G';
+        WHEN 43 THEN SET result = 'H';
+        WHEN 44 THEN SET result = 'I';
+        WHEN 45 THEN SET result = 'J';
+        WHEN 46 THEN SET result = 'K';
+        WHEN 47 THEN SET result = 'L';
+        WHEN 48 THEN SET result = 'M';
+        WHEN 49 THEN SET result = 'N';
+        WHEN 50 THEN SET result = 'O';
+        WHEN 51 THEN SET result = 'P';
+        WHEN 52 THEN SET result = 'Q';
+        WHEN 53 THEN SET result = 'R';
+        WHEN 54 THEN SET result = 'S';
+        WHEN 55 THEN SET result = 'T';
+        WHEN 56 THEN SET result = 'U';
+        WHEN 57 THEN SET result = 'V';
+        WHEN 58 THEN SET result = 'W';
+        WHEN 59 THEN SET result = 'X';
+        WHEN 60 THEN SET result = 'Y';
+        WHEN 61 THEN SET result = 'Z';
+	END CASE;
+    RETURN result;
+END$$
+
+DELIMITER ;
+
 -- --------------------------------------------------------
 
 --
@@ -29,17 +128,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `access` (
-  `key_url` varchar(6) CHARACTER SET utf32 COLLATE utf32_bin NOT NULL,
+  `key_url` varchar(6) COLLATE utf32_bin NOT NULL,
   `browser` tinyint(1) NOT NULL,
-  `clicked_time` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf32;
-
---
--- Dumping data for table `access`
---
-
-INSERT INTO `access` (`key_url`, `browser`, `clicked_time`) VALUES
-('00000B', 0, '1501221064');
+  `clicked_time` text COLLATE utf32_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_bin;
 
 -- --------------------------------------------------------
 
@@ -48,53 +140,23 @@ INSERT INTO `access` (`key_url`, `browser`, `clicked_time`) VALUES
 --
 
 CREATE TABLE `url` (
-  `key_url` char(6) CHARACTER SET utf32 COLLATE utf32_bin NOT NULL,
-  `original_link` text CHARACTER SET utf32 COLLATE utf32_bin NOT NULL,
-  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+  `id` bigint(20) NOT NULL,
+  `key_url` varchar(6) COLLATE utf32_bin NOT NULL,
+  `original_link` text COLLATE utf32_bin NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_bin;
 
 --
--- Dumping data for table `url`
+-- Triggers `url`
 --
-
-INSERT INTO `url` (`key_url`, `original_link`, `created_time`) VALUES
-('000000', 'https://bozuman.cybozu.com/g/schedule/index.csp?', '2017-07-26 07:24:10'),
-('000001', 'http://localhost/phpmyadmin/tbl_change.php?db=cus&table=url', '2017-07-26 07:24:35'),
-('000002', 'https://github.com/namnguyen95?tab=overview&from=2017-07-01&to=2017-07-26', '2017-07-26 09:57:11'),
-('000003', 'https://github.com/', '2017-07-27 02:12:09'),
-('000004', 'https://translate.google.com/?hl=vi', '2017-07-27 02:32:16'),
-('000005', 'https://github.com/cvn-intern-2017/cus', '2017-07-27 03:21:24'),
-('000006', 'http://bongdaplus.vn/', '2017-07-27 05:57:53'),
-('000007', 'https://ko.wikipedia.org/wiki/%EC%9C%84%ED%82%A4%EB%B0%B1%EA%B3%BC:%EB%8C%80%EB%AC%B8', '2017-07-27 06:08:45'),
-('000008', 'https://bozuman.cybozu.com/g/schedule/view.csp?event=1259059&bdate=2017-07-28&uid=1854&gid=462&referer_key=f9c8e58b62d73e02eed0d3f977d08c90&start_day=&end_day=', '2017-07-27 06:12:51'),
-('000009', 'https://www.facebook.com/', '2017-07-27 06:18:52'),
-('00000A', 'https://github.com/liubin/fluentd-agent', '2017-07-28 05:49:06'),
-('00000B', 'https://github.com/cvn-intern-2017/cus/graphs/contributors', '2017-07-28 05:49:50'),
-('00000a', 'https://abc.com', '2017-07-27 06:20:30'),
-('00000b', 'https://github.com/cvn-intern-2017/cus/blob/master/config/cus.sql', '2017-07-27 06:31:38'),
-('00000c', 'https://www.digitalocean.com/community/tutorials/how-to-install-linux-apache-mysql-php-lamp-stack-on-ubuntu-16-04', '2017-07-27 06:53:19'),
-('00000d', 'https://bozuman.cybozu.com/g/schedule/view.csp?uid=1854&event=1274113&bdate=2017-07-27&nid=31651020', '2017-07-27 06:55:30'),
-('00000e', 'https://www.chiark.greenend.org.uk/~sgtatham/putty/latest.html', '2017-07-27 06:58:22'),
-('00000f', 'http://localhost/phpmyadmin/sql.php?db=cus&goto=db_structure.php&table=url&pos=0', '2017-07-27 07:11:55'),
-('00000g', 'https://bozuman.cybozu.com/g/schedule/mobile/personal_week.csp?', '2017-07-27 07:14:51'),
-('00000h', 'https://bozuman.cybozu.com/g/schedule/mobile/group_day.csp?bdate=2017-07-27&uid=1854&gid=462&search_text=', '2017-07-27 07:16:38'),
-('00000i', 'https://bozuman.cybozu.com/g/schedule/mobile/group_day.csp?bdate=2017-07-28&uid=&gid=462&event=&event_date=&sp=0&search_text=&uids=&fids=', '2017-07-27 07:18:00'),
-('00000j', 'http://localhost/phpmyadmin/sql.php?db=cus&goto=db_structure.php&table=url&po', '2017-07-27 07:18:45'),
-('00000k', 'http://cus.dev.cybozu.xyz/', '2017-07-27 07:20:06'),
-('00000l', 'http://materializecss.com/forms.html', '2017-07-27 07:30:45'),
-('00000m', 'http://localhost/phpmyadmin/sql.php?db=cus&goto=db_structure.php', '2017-07-27 07:47:19'),
-('00000n', 'http://php.net/manual/en/function.parse-ini-file.php', '2017-07-28 04:29:58'),
-('00000o', 'https://www.google.co.jp/?gfe_rd=cr&ei=n7h6WYqWKqrz8Ae2w4eADA', '2017-07-28 04:30:33'),
-('00000p', 'http://qiita.com/liubin/items/3722ab10a73154863bd4', '2017-07-28 04:31:27'),
-('00000q', 'http://cus.dev.cybozu.xyz/00000o', '2017-07-28 04:37:13'),
-('00000r', 'http://cus.dev.cybozu.xyz/00000z', '2017-07-28 04:37:25'),
-('00000s', 'https://stackoverflow.com/questions/37239970/connect-to-mysql-server-without-sudo', '2017-07-28 04:40:39'),
-('00000t', 'http://qiita.com/liubin/items/92a4e7e3917143ae4aaf', '2017-07-28 04:41:12'),
-('00000u', 'https://github.com/cvn-intern-2017/cus/wiki/Specification:-Feature-1---Shorten-URL', '2017-07-28 04:41:27'),
-('00000v', 'http://cus.dev.cybozu.xyz/00000', '2017-07-28 04:41:41'),
-('00000w', 'http://cus.dev.cybozu.xyz/00000v', '2017-07-28 04:41:45'),
-('00000x', 'http://cus.dev.cybozu.xyz/00000w', '2017-07-28 04:41:52'),
-('00000z', 'http://cus.dev.cybozu.xyz/00000y', '2017-07-28 04:42:00');
+DELIMITER $$
+CREATE TRIGGER `set_key_url` BEFORE INSERT ON `url` FOR EACH ROW BEGIN
+   DECLARE next_id BIGINT;
+   SET next_id = (SELECT MAX(id) FROM `url`) + 1;
+   SET NEW.key_url=convert_10_to_62(next_id);
+END
+$$
+DELIMITER ;
 
 --
 -- Indexes for dumped tables
@@ -104,25 +166,23 @@ INSERT INTO `url` (`key_url`, `original_link`, `created_time`) VALUES
 -- Indexes for table `access`
 --
 ALTER TABLE `access`
-  ADD PRIMARY KEY (`key_url`,`browser`),
-  ADD KEY `index_browser` (`browser`);
+  ADD PRIMARY KEY (`key_url`,`browser`);
 
 --
 -- Indexes for table `url`
 --
 ALTER TABLE `url`
-  ADD PRIMARY KEY (`key_url`);
+  ADD PRIMARY KEY (`id`,`key_url`);
 
 --
--- Constraints for dumped tables
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- Constraints for table `access`
+-- AUTO_INCREMENT for table `url`
 --
-ALTER TABLE `access`
-  ADD CONSTRAINT `access_ibfk_1` FOREIGN KEY (`key_url`) REFERENCES `url` (`key_url`);
-COMMIT;
+ALTER TABLE `url`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
